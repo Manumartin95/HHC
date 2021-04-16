be3122b682f3b025c44a2c2e5b529a9cdd97134c
mmartin <manuel-harley@hotmail.com>
2021-04-16 16:36:40 +0100

    Final commit - new images

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const world = 'world';
-console.log(`Hello ${world}`);
-
 document.getElementById('review-button').addEventListener('click', function(){
     let element = document.getElementById('thanks-text')
     element.style.display = "revert";
 })
-
